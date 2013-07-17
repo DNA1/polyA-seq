cb68248164fc779c1639136477a9eaea78de5c40
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2013-07-16 22:21:21 -0400

    It now prints properly, and recognizes both capital and lower case Ts

--- polyA/src/FilterTool.java
+++ polyA/src/FilterTool.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class FilterTool {
 	int _x;
 	
-	public FilterTool(String firstEndPath, String secondEndPath, int numberToDetect, String outputFileName) throws IOException {
+	public FilterTool(String firstEndPath, String secondEndPath, int numberToDetect, String outputFileName) throws IOException{
 		
 		_x = numberToDetect;
 		
@@ -15,35 +15,40 @@ public class FilterTool {
 		File outputFile2 = new File(outputFileName+"2");
 		Writer outputWriter2 = new BufferedWriter(new FileWriter(outputFile2));
 		
-		File firstEnd = new File(firstEndPath);
-		File secondEnd = new File(secondEndPath);
-		
-		Scanner firstEndScanner = new Scanner(firstEnd);
-		Scanner secondEndScanner = new Scanner(secondEnd);
-		
-		
-		while(secondEndScanner.hasNext()){
-			String secondSequence = null;
-			String secondRead =secondEndScanner.next();
-			secondSequence = secondEndScanner.next();
-			secondRead = secondRead + "\n"+ secondSequence;
-			secondRead = secondRead + "\n"+ secondEndScanner.next();
-			secondRead = secondRead + "\n"+ secondEndScanner.next();
+		try {
+			File firstEnd = new File(firstEndPath);
+			File secondEnd = new File(secondEndPath);
 			
-			String firstSequence = null;
-				String firstRead =firstEndScanner.next();
-				firstSequence = firstEndScanner.next();
-				firstRead = firstRead + "\n"+ firstSequence;
-				firstRead = firstRead + "\n"+ firstEndScanner.next();
-				firstRead = firstRead + "\n"+ firstEndScanner.next();
+			Scanner firstEndScanner = new Scanner(firstEnd);
+			Scanner secondEndScanner = new Scanner(secondEnd);
 			
-			if (tCounter(secondSequence) == _x) {
+			
+			while(secondEndScanner.hasNext()){
+				String secondSequence = null;
+				String secondRead =secondEndScanner.next();
+				secondSequence = secondEndScanner.next();
+				secondRead = secondRead + "\n" + secondSequence + "\n";
+				secondRead = secondRead + secondEndScanner.next() + "\n";
+				secondRead = secondRead + secondEndScanner.next();
 				
-				outputWriter1.write("\n"+firstRead);
-				outputWriter2.write("\n"+secondRead);
+				String firstSequence = null;
+				String firstRead = firstEndScanner.next();
+				firstSequence = firstEndScanner.next();
+				firstRead = firstRead + "\n" + firstSequence + "\n";
+				firstRead = firstRead + firstEndScanner.next() + "\n";
+				firstRead = firstRead + firstEndScanner.next();
+				
+				if (tCounter(secondSequence) == _x) {
+					
+					outputWriter1.write(firstRead + "\n");
+					outputWriter2.write(secondRead + "\n");
+				}
 			}
+			
+		} 
+		catch (FileNotFoundException e) {
+			e.getMessage();
 		}
-		
 		
 		outputWriter1.close();
 		outputWriter2.close();
@@ -54,7 +59,7 @@ public class FilterTool {
 		int count = 0;
 		for (int i=0; i<_x ;i++){
 			Character b = sequence.charAt(i);
-			if (b.equals('T')) {
+			if (b.equals('T') || b.equals('t')) {
 				count++;
 			}
 		}
